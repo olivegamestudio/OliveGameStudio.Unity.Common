9099897076d756da903a20ee31144fdd0bcda8c2
Simon Jefferies <simon@olivegame.studio>
2025-01-23 21:16:24 +0000

    Add RotateBehaviour mono behaviour.

---
+++ RotateBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Handles the rotation of a game object at a specified speed.
+/// </summary>
+public class RotateBehaviour : MonoBehaviour
+{
+    /// <summary>
+    /// The speed of rotation.
+    /// </summary>
+    public float Speed;
+
+    /// <summary>
+    /// Called once per frame to update the rotation of the game object.
+    /// </summary>
+    void Update()
+    {
+        gameObject.transform.Rotate(0, 0, Speed * Time.deltaTime);
+    }
+}
